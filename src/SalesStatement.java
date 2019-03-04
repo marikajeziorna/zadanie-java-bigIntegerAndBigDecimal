1debb85efe64af56d8e002aa114636106d701461
marika.jeziorna <marika.jeziorna@collibra.com>
2019-03-04 09:19:34 +0100

    zadanie-BigDecimal refactor method calcVat

--- src/SalesStatement.java
+++ src/SalesStatement.java
@@ -24,7 +24,7 @@ public class SalesStatement {
     }
 
    public BigDecimal calcNett(){
-     return new BigDecimal (grossPrice / vatRate);
+     return new BigDecimal (grossPrice * vatRate/100);
    }
 }
 
